60cb0b56e7bc4f5096696a339cef74485691d886
milennne <milene.fuentes@tecsup.edu.pe>
2025-09-13 22:58:11 -0500

    COMMIT DE MILENE

--- app/src/main/java/com/example/desafio1/MainActivity.kt
+++ app/src/main/java/com/example/desafio1/MainActivity.kt
@@ -5,65 +5,68 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.desafio1.ui.theme.DESAFIO1Theme
-import com.example.desafio1.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TutorialScreen()
+            DESAFIO1Theme {
+                TutorialScreen()
+            }
         }
     }
 }
 
 @Composable
-fun TutorialScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+fun TutorialScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
         Image(
-            painter = painterResource(id = R.drawable.bg_compose_background),
+            painter = painterResource(R.drawable.bg_compose_background),
             contentDescription = null,
             modifier = Modifier.fillMaxWidth()
         )
-
         Text(
-            text = stringResource(id = R.string.title),
+            text = "Jetpack Compose tutorial",
             fontSize = 24.sp,
             modifier = Modifier.padding(16.dp)
         )
-
         Text(
-            text = stringResource(id = R.string.paragraph1),
+            text = "Jetpack Compose is a modern toolkit for building native Android UI. " +
+                    "Compose simplifies and accelerates UI development on Android with less code, " +
+                    "powerful tools, and intuitive Kotlin APIs.",
             textAlign = TextAlign.Justify,
             modifier = Modifier.padding(start = 16.dp, end = 16.dp)
         )
-
         Text(
-            text = stringResource(id = R.string.paragraph2),
+            text = "In this tutorial, you build a simple UI component with declarative functions. " +
+                    "You call Compose functions to say what elements you want and the Compose compiler does the rest. " +
+                    "Compose is built around Composable functions. These functions let you define your app's UI " +
+                    "programmatically because they let you describe how it should look and provide data dependencies, " +
+                    "rather than focus on the process of the UI's construction, such as initializing an element and " +
+                    "then attaching it to a parent. To create a Composable function, you add the @Composable annotation " +
+                    "to the function name.",
             textAlign = TextAlign.Justify,
             modifier = Modifier.padding(16.dp)
         )
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun TutorialScreenPreview() {
-    TutorialScreen()
+fun TutorialPreview() {
+    DESAFIO1Theme {
+        TutorialScreen()
+    }
 }
